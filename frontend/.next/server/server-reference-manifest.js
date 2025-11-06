cff38357c2e886821daa38a36bc8be5bac5c83f9
hal7716 <m.haru.7160@gmail.com>
2025-11-06 16:23:59 +0700

    Add node_modules to gitignore

--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FLJ+/iYYs5TRkc+ZKgP8Lt6ACJXVooMfAR3pkmQ08e0=\"\n}"
--- frontend/.next/static/chunks/src_app_ranking_page_jsx_77525471._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_b955d608._.js",
-  "static/chunks/src_app_4829bde9._.js",
-  "static/chunks/src_app_ranking_ranking_module_ba86e66b.css"
-],
-    source: "dynamic"
-});
