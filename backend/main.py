c41585225c19ab130dc4d45625312e32681a3a61
hal7716 <m.haru.7160@gmail.com>
2025-11-07 21:54:14 +0700

    authenication

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from supabase import create_client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI connected to Supabase"}
